1d8a709ac8abe0dc7279878d858c0c548e49bdda
rtorres <rene.torres@csps-efpc.gc.ca>
2023-10-13 11:50:55 -0400

    Add toggle btn title translations

--- src/i18n/messages.js
+++ src/i18n/messages.js
@@ -1,51 +1,58 @@
 ﻿export default {
-    en: {
-      quiz: {
-        loading: "Loading quiz data...",
-        results: "Quiz Results",
-        score: "You scored {0} out of {1}. Your percentage is {2}%",
-        passed: "Congratulations! You passed the quiz.",
-        failed: "Sorry, you did not pass the quiz.",
-        completed: "You have completed the quiz. You may navigate away from this page.",
-      },
-      button: {
-        getFeedback: "Get the Quiz Feedback",
-        completeQuiz: "Complete Quiz",
-      },
-      pageInfo: "Page {0} of {1}",
-      question: {
-        submit: "Validate",
-        retry: "Retry",
-        previous: "Previous",
-        next: "Next",
-        pleaseSelectAnswer: "Please select an answer before validating.",
-        pleaseSelectAtLeastOneAnswer: "Please select at least one answer before validating.",
-        pleaseSelectEveryTerm: "Please select every term before validating",
-      },
-    },
-    fr: {
-      quiz: {
-        loading: "Chargement des données du quiz...",
-        results: "Résultats du quiz",
-        score: "Vous avez obtenu {0} sur {1}. Votre pourcentage est de {2}%",
-        passed: "Félicitations! Vous avez réussi le quiz.",
-        failed: "Désolé, vous n'avez pas réussi le quiz.",
-        completed: "Vous avez terminé le quiz. Vous pouvez quitter cette page.",
-      },
-      button: {
-        getFeedback: "Obtenir les commentaires du quiz",
-        completeQuiz: "Terminer le quiz",
-      },
-      pageInfo: "Page {0} sur {1}",
-      question: {
-        submit: "Valider",
-        retry: "Réessayer",
-        previous: "Précédent",
-        next: "Suivant",
-        pleaseSelectAnswer: "Veuillez sélectionner une réponse avant de valider.",
-        pleaseSelectAtLeastOneAnswer: "Veuillez sélectionner au moins une réponse avant de valider.",
-        pleaseSelectEveryTerm: "Veuillez sélectionner chaque terme avant de valider",
-        },
-    },
-  };
-  
+	en: {
+		quiz: {
+			loading: 'Loading quiz data...',
+			results: 'Quiz Results',
+			score: 'You scored {0} out of {1}. Your percentage is {2}%',
+			passed: 'Congratulations! You passed the quiz.',
+			failed: 'Sorry, you did not pass the quiz.',
+			completed:
+				'You have completed the quiz. You may navigate away from this page.',
+		},
+		button: {
+			getFeedback: 'Get the Quiz Feedback',
+			completeQuiz: 'Complete Quiz',
+			langToggle: 'Changer la langue de la page',
+		},
+		pageInfo: 'Page {0} of {1}',
+		question: {
+			submit: 'Validate',
+			retry: 'Retry',
+			previous: 'Previous',
+			next: 'Next',
+			pleaseSelectAnswer: 'Please select an answer before validating.',
+			pleaseSelectAtLeastOneAnswer:
+				'Please select at least one answer before validating.',
+			pleaseSelectEveryTerm: 'Please select every term before validating',
+		},
+	},
+	fr: {
+		quiz: {
+			loading: 'Chargement des données du quiz...',
+			results: 'Résultats du quiz',
+			score: 'Vous avez obtenu {0} sur {1}. Votre pourcentage est de {2}%',
+			passed: 'Félicitations! Vous avez réussi le quiz.',
+			failed: "Désolé, vous n'avez pas réussi le quiz.",
+			completed:
+				'Vous avez terminé le quiz. Vous pouvez quitter cette page.',
+		},
+		button: {
+			getFeedback: 'Obtenir les commentaires du quiz',
+			completeQuiz: 'Terminer le quiz',
+			langToggle: 'Change page language',
+		},
+		pageInfo: 'Page {0} sur {1}',
+		question: {
+			submit: 'Valider',
+			retry: 'Réessayer',
+			previous: 'Précédent',
+			next: 'Suivant',
+			pleaseSelectAnswer:
+				'Veuillez sélectionner une réponse avant de valider.',
+			pleaseSelectAtLeastOneAnswer:
+				'Veuillez sélectionner au moins une réponse avant de valider.',
+			pleaseSelectEveryTerm:
+				'Veuillez sélectionner chaque terme avant de valider',
+		},
+	},
+};
